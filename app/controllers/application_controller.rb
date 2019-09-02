4c4cd59637febbb474e761d70dba62bfb90c12c6
Amit Rawat <beingamit4197@gmail.com>
2019-09-02 13:02:46 +0530

    add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: exception
+    protect_from_forgery with: :exception
+  
     def hello
-       refer html: "Hello Amit"
+      render html: "hello, world!"
     end
-end
+  end
