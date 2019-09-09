93d67f2b3bccc53c00e22a8f0f6a201ea3b67f38
Amit Rawat <beingamit4197@gmail.com>
2019-09-09 14:19:38 +0530

    layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/contact'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/home'
-
+  get 'users/new'
   root 'static_pages#home'
+  get '/contact', to: "static_pages#contact"
+  get '/help', to: "static_pages#help"
+  get '/about', to: "static_pages#about"
+  get '/home', to: "static_pages#home"
+  get '/login', to: "static_pages#home"
+
 end
